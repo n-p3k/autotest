6c4fb84c6df892bca3213bd4e62d28671120446b
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 17:05:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+"""
+This module handle reduce app
+ 
+Functions: reduce(alpha, beta)
+"""
 def reduce(variable_alpha, variable_beta):
     """Takes in two numbers, returns their product."""
     return variable_alpha + variable_beta
@@ -8,5 +12,5 @@ def app():
     print('hello we reduced to :', reduce(1, 2))
 
 if __name__=="__main__":
-    """Main."""
+    """Main loop."""
     app()
