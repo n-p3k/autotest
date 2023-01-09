36477d8f5f4e822dc3cea1e37d5d8b8b0f7823f1
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 18:19:55 -0800

    Create app.py

---
+++ module/app.py
@@ -0,0 +1,12 @@
+"""
+This module handle reduce app
+Functions: reduce(alpha, beta)
+"""
+def reduce(variable_alpha, variable_beta):
+    """Takes in two numbers, returns their product."""
+    return variable_alpha + variable_beta
+
+def run():
+    """Run the app."""
+    print('hello we reduced to :', reduce(1, 2))
+    return True
