188079bd3df27ba2f137480849c25b7119213aac
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 18:12:05 -0800

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ Test module for app
 
 Classes: TestApp()
 """
-import test_env
+import app
 
 # content of test_class_demo.py
 class TestApp:
@@ -23,4 +23,4 @@ class TestApp:
 
     def test_two(self):
         """Test two"""
-        assert test_env.module_app().app()
+        assert app.app()
