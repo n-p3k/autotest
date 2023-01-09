3ae163df63a68ed703b3ebfc5213b1e6c9621e0f
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 18:17:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ This module handle reduce app
 
 Functions: reduce(alpha, beta)
 """
-def reduce(variable_alpha, variable_beta):
-    """Takes in two numbers, returns their product."""
-    return variable_alpha + variable_beta
-
-def app():
-    """Run the app."""
-    print('hello we reduced to :', reduce(1, 2))
-    return True
+from app import app
 
 if __name__=="__main__":
     # main loop
