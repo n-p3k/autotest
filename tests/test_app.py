5964ba032fd9d7547bf59ed8d4e4e7db59994c7a
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 17:14:31 -0800

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+# content of test_sysexit.py
+import pytest
+import main
+
+# content of test_class_demo.py
+class TestApp:
+    def test_one(self):
+        assert True
+
+    def test_two(self):
+        assert app()
