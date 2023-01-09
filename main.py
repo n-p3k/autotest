ab18542686ccd699db28cb515476d32a037e8ab9
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 16:57:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 def reduce(a, b):
+    """Takes in two numbers, returns their product."""
     return a + b
 
 def app():
-  print('hello we reduced to :', reduce(1, 2))
+    """Main app."""
+    print('hello we reduced to :', reduce(1, 2))
 
 if __name__=="__main__":
-  app()
+    app()
