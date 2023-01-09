7d013f3e12b6ec581676b3645aec553289df93e6
nora-p3k <107825492+n-p3k@users.noreply.github.com>
2023-01-08 18:51:37 -0800

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,21 @@
+from app import app
+
+
+class TestApp:
+    """
+    A class to test the app.
+    Methods
+    -------
+    test_one():
+        First test.
+     test_two():
+        Second test.
+    """
+    def test_one(self):
+        """Test one"""
+        print(myapp_env.sys)
+        assert True
+
+    def test_two(self):
+        """Test two"""
+        assert app.run()
